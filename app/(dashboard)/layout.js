543c91772bb688cd58c66619ad3374e09924b36f
Saqlain Sultan <saqlainsultan991@gmail.com>
2024-04-05 19:07:06 +0530

    Final Comit

--- app/(dashboard)/layout.js
+++ app/(dashboard)/layout.js
@@ -1,35 +1,56 @@
-"use client"
-import React, { useState } from 'react'
-import SideNav from './_components/SideNav'
-import TopHeader from './_components/TopHeader'
-import Toast from '../_components/Toast';
+"use client";
+import React, { useState, useEffect } from "react";
+import SideNav from "./_components/SideNav";
+import TopHeader from "./_components/TopHeader";
+import Toast from "../_components/Toast";
+import Footer from "../_components/Footer";
 
-function layout({ children }) {
+function Layout({ children }) {
   const [toggle, setToggle] = useState(false);
-  
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (event.target.closest(".side-nav")) return;
+      setToggle(false);
+    };
+
+    document.body.addEventListener("click", handleClickOutside);
+
+    return () => {
+      document.body.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
   return (
-    // <ToastContext.Provider value={{toast,setToast}}>
-    <div>
-      <div className=' h-full w-64 flex-col 
-        fixed inset-y-0 z-30 md:flex hidden bg-white'>
-        <SideNav closeSideBar={()=>setToggle(false)}/>
-      </div>
-   
-      {toggle ? <div className=' h-full w-64 flex-col 
-        fixed inset-y-0 z-30 bg-white flex'>
-        <SideNav closeSideBar={()=>setToggle(false)}/>
-      </div> : null}
+    <>
+      <div>
+        <div
+          className="h-full w-64 flex-col 
+        fixed inset-y-0 z-30 md:flex hidden bg-white"
+        >
+          <SideNav closeSideBar={() => setToggle(false)} />
+        </div>
 
-      <div className='md:ml-64'>
-        <TopHeader setToggleBar={() => 
-          setToggle(true)} />
-        {children}
-        {/* <Toast/> */}
-      </div>
+        {toggle ? (
+          <div
+            className="h-full w-64 flex-col 
+        fixed inset-y-0 z-30 bg-white flex"
+          >
+            <SideNav closeSideBar={() => setToggle(false)} />
+          </div>
+        ) : null}
 
-    </div>
-    
-  )
+        <div className="md:ml-64">
+          <TopHeader setToggleBar={() => setToggle(true)} />
+          {children}
+          {/* <Toast/> */}
+          <div className="">
+            <Footer />
+          </div>{" "}
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default layout
+export default Layout;
